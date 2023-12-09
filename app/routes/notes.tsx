ab12b6fa36c2514e79c6fefb65fd031c619140b8
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 19:41:32 +0500

    action method

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,9 +1,16 @@
 import NewNote from "~/components/Notes";
 
-export default function notes() {
+export default function NotesPage() {
   return (
     <div>
-        <NewNote />
+      <NewNote />
     </div>
-  )
+  );
+}
+
+
+// just like links() function for CSS this remix will looks for this aciton function and this action function will runs for backend code
+// and action function will ONLY runs for a NON-GET request means (post, put or delete). Action function will NOT runs for GET method
+export function action() {
+    
 }
