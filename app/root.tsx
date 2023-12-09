25dfff411f7978082dbbcdf60073f451d7ad59e4
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 23:42:33 +0500

    error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -9,6 +10,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 // this ~ means starts from app route like app/components/Header
+import { useRouteError } from "@remix-run/react";
 import MainNavigation from "~/components/Header";
 
 export const links: LinksFunction = () => [
@@ -36,4 +38,41 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+// if any error occured
+export function ErrorBoundary() {
+  const error: any = useRouteError();
+  return (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta />
+      <Links />
+      <title>An Error Occured!</title>t
+    </head>
+    <body>
+      <header>
+        {/* Show nav buttons at top on each page */}
+        <MainNavigation />
+      </header>
+      <div>
+        <h1>
+          An Error Occured!
+        </h1>
+        <p>
+          {error?.message}
+        </p>
+        <p>
+          Back to <Link to='/'>Home Page</Link>
+        </p>
+      </div>
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>
+);
+
 }
