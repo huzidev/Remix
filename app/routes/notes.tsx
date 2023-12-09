a8e90386e4b5314727632d7ea85349cebf02153c
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 20:06:32 +0500

    form data

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,3 +1,4 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
 import NewNote from "~/components/Notes";
 
 export default function NotesPage() {
@@ -10,6 +11,13 @@ export default function NotesPage() {
 
 // just like links() function for CSS this remix will looks for this aciton function and this action function will runs for backend code
 // and action function will ONLY runs for a NON-GET request means (post, put or delete). Action function will NOT runs for GET method
-export function action() {
-    
+export async function action({ request }: ActionFunctionArgs) {
+    // will receive form submitted data from ours form
+    const formData = await request.formData();
+    // const noteData = {
+    //     title: formData.get('title'),
+    //     conent: formData.get('content')
+    // }
+    // OR
+    const noteData = Object.entries(formData);
 }
