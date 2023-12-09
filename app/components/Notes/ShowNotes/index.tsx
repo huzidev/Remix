773dd8a4e88a93eb0d4918c1ee4d0b662e6c395d
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 21:52:39 +0500

    show notes page

---
+++ app/components/Notes/ShowNotes/index.tsx
@@ -0,0 +1,21 @@
+import { Notes } from "~/data/types";
+
+export default function ShowNotes({ notes }) {
+  return (
+    <ul id="note-list">
+      {notes.map((note: Notes, index: number) => (
+        <li key={note.id} className="note">
+          <article>
+            <header>
+              <ul className="note-meta">
+                <li>#{index + 1}</li>
+              </ul>
+              <h2>{note.title}</h2>
+            </header>
+            <p>{note.content}</p>
+          </article>
+        </li>
+      ))}
+    </ul>
+  );
+}
