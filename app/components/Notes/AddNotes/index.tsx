4511349ca85e0f19c4828ba5b051e866a80ba51e
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 22:11:42 +0500

    default form data

--- app/components/Notes/AddNotes/index.tsx
+++ app/components/Notes/AddNotes/index.tsx
@@ -1,10 +1,12 @@
+import { Form } from "@remix-run/react";
+
 export default function NewNote() {
   return (
     // like in react we do onSubmit={submitHandler} and submitHandler function will prevent the event.preventDefault
     // no need for this in remix
     // also no need to define the action='/notes' because the request will be send to the current active path and this component
     // will be run on /notes path
-    <form method="post" id="note-form">
+    <Form method="post" id="note-form">
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
@@ -16,6 +18,6 @@ export default function NewNote() {
       <div className="form-actions">
         <button>Add Note</button>
       </div>
-    </form>
+    </Form>
   );
 }
