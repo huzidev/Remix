859115d4488047b2e894df61d9820944fdd7c359
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 19:27:34 +0500

    add note component

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,9 +1,11 @@
+import NewNote from "~/components/Notes";
 
 export default function notes() {
   return (
     <div>
         <h1>
             Notes
+            <NewNote />
         </h1>
     </div>
   )
