7c0926c72a2e8fbaf56584a73a1a802fb17e53e0
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 19:14:34 +0500

    if active or pending

--- app/components/MainNavigation.tsx
+++ app/components/MainNavigation.tsx
@@ -1,16 +1,19 @@
 import { NavLink } from "@remix-run/react";
+import * as endpoints from "~/endpoints/index";
 
 export default function MainNavigation() {
   return (
     <div id="main-navigation">
         <ul>
             <li className="nav-item">
-                <NavLink to="/">
+                {/* NavLink used instead of Link because NavLink provides active class and also checks if it's in pending state
+                so it'll be easier to put class if state is in pending or in active state */}
+                <NavLink to={endpoints.HOME}>
                     Home
                 </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink to="/notes">
+                <NavLink to={endpoints.NOTES}>
                     Notes
                 </NavLink>;
             </li>
