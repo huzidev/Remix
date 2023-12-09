512853d99c1dab67fea1e924d8857a2867798c45
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 19:20:03 +0500

    index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,11 @@
 import { Link } from "@remix-run/react";
+import * as endpoints from '~/endpoints/index';
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Notes</h1>
-      <Link to='/notes'>Notes</Link>
+      <h1>Welcome to My Notes App</h1>
+      <p><Link to={endpoints.NOTES}>Try</Link> creating your own Notes</p>
     </div>
   );
 }
