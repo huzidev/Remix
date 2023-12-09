9540302858fe1bfed4d590f22b65c571fd2247f8
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 20:07:27 +0500

    on submit request function

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -11,6 +11,7 @@ export default function NotesPage() {
 
 // just like links() function for CSS this remix will looks for this aciton function and this action function will runs for backend code
 // and action function will ONLY runs for a NON-GET request means (post, put or delete). Action function will NOT runs for GET method
+// action function will receive data object data.request or we can simply destructure it and use { request }
 export async function action({ request }: ActionFunctionArgs) {
     // will receive form submitted data from ours form
     const formData = await request.formData();
