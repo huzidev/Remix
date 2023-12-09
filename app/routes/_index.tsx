02ddd650c46615baf4f4804f8ad19da0db8337fe
huzidev <huzaifa.iqdev@gmail.com>
2023-12-09 19:25:36 +0500

    add note page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import * as endpoints from '~/endpoints/index';
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div id="content">
       <h1>Welcome to My Notes App</h1>
       <p><Link to={endpoints.NOTES}>Try</Link> creating your own Notes</p>
     </div>
