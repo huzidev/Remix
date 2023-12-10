cd9138c44ab5a6e19e008a8ac1e71a6ed0621313
huzidev <huzaifa.iqdev@gmail.com>
2023-12-10 13:34:13 +0500

    get single notes

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,8 +1,20 @@
+import { useLoaderData } from "@remix-run/react";
+import { getStoredNotes } from "~/data/notes";
+import { Notes } from "~/data/types";
+
 export default function NotePage() {
+  const data: Notes = useLoaderData();
+
   return (
     <div>
-      <h1>Note Title</h1>
-      <p>Note Content</p>
+      <p>{data.id}</p>
+      <h1>{data.title}</h1>
+      <p>{data.content}</p>
     </div>
   );
+}
+
+export async function loader(id: number) {
+  const note = await getStoredNotes(id);
+  return note;
 }
